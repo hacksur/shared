2521e501dd2be947ee9aeb26e46922ae8d5209cc
Julian Clatro <jclatro@yahoo.com>
2023-03-27 13:32:48 -0300

    init

---
+++ src/components/Uploader/Uploader.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Button } from '../Button';
+
+type UploaderProps = {
+  text: string;
+  to: string;
+  submit: any;
+  size?: 'tiny' | 'small' | 'medium' | 'large';
+  icon?: string | undefined;
+  variant?:
+  | 'primary'
+  | 'secondary'
+  | 'basic'
+  | 'outline'
+  | 'alert'
+  | 'warning'
+  | 'positive'
+  | 'dropdown';
+  customCss: string;
+}
+
+export const Uploader: React.FunctionComponent<UploaderProps> = ({ text, submit, to, variant, size, icon, customCss}) => {
+  const startUpload = async (event: any) => {
+    if (!!event) {
+      const file = event.target.files[0];
+      const formData = new FormData();
+      formData.append('file', new Blob([await file.arrayBuffer()], { type: 'image/png' }), 'file.png');
+      submit(formData, {
+        method: 'post',
+        action: to,
+        encType: 'multipart/form-data',
+      });
+    }
+  };
+  
+  return <Button type="upload" text={text} startUpload={startUpload} variant={variant} size={size} icon={icon} customCss={customCss} />
+};
---
+++ src/helpers/index.ts
@@ -0,0 +1,3 @@
+export { FakeLink } from "./FakeLink";
+export { experienceCards } from "./constants";
+export { isEqual } from './isEqual';
